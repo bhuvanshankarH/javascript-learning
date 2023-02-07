0d1d7edcfacc2e14c968b28000375c67eceb6ba3
bhuvanshankarH <shankarbhuvan115@gmail.com>
2023-02-07 07:35:12 +0530

    done with variables

---
+++ script.js
@@ -0,0 +1,23 @@
+// alert("Im working"); // it shows an dialogue box iver the window 
+
+// Variables 
+
+// variables are like storage box which is used to store data for operations 
+
+// let person = "Bhuvan"  es6 version and it is also variable / changable
+
+// const person = "Bhuvan" once declared cannot be reassigned 
+
+// var person = "Bhuvan"  old version not recommended 
+
+//  !!! note use const more than let because it is recommended !!!
+
+// rules for naming the variable 
+
+// variable must need to start with alphabets $ and _ signs 
+
+//  Variables should not start  with numbers 
+
+//  variable should not be assigned with keyword
+
+
